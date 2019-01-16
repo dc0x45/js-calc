d1a5966bce592f11143076fcab5095a4a7309e0f
dcote007 <32779137+dcote007@users.noreply.github.com>
2019-01-15 21:16:45 -0500

    Update calc.js

--- calc.js
+++ calc.js
@@ -7,13 +7,11 @@ function parseNum(num){
     if (lastNum){
       nonLoc.push(num);
       let outNum = Number(nonLoc.join('')).toLocaleString();
-      document.getElementById('output').value = (outNum);
-      lastSym = true; numNums++;
+      document.getElementById('output').value = (outNum); lastSym = true; numNums++;
     } else {
       nonLoc.push(num);
       let outNum = Number(nonLoc.join('')).toLocaleString();
-      document.getElementById('output').value = (outNum);
-      lastSym = true; lastNum = true; numNums++;
+      document.getElementById('output').value = (outNum); lastSym = true; lastNum = true; numNums++;
     }
   evalArr[currNum] = evalArr[currNum] + num;
   }
@@ -23,13 +21,11 @@ function parseDec(){
     if (lastNum){
       nonLoc.push('.');
       document.getElementById('output').value = document.getElementById('output').value + '.';
-      evalArr[currNum] = evalArr[currNum] + '.';
-      lastSym = true; numNums++;
+      evalArr[currNum] = evalArr[currNum] + '.'; lastSym = true; numNums++;
     } else {
       nonLoc.push('.');
       document.getElementById('output').value = document.getElementById('output').value + '.';
-      evalArr[currNum] = evalArr[currNum] + '.';
-      lastSym = true; lastNum = true; numNums++;
+      evalArr[currNum] = evalArr[currNum] + '.'; lastSym = true; lastNum = true; numNums++;
     }
   }
 }
@@ -39,15 +35,17 @@ function parseSym(sym){
       document.getElementById('output').value = (sym); nonLoc = []; numNums = 0;
       lastSym = false; lastNum = false; lastEql = false; evalArr[1] = evalArr[1] + sym; currNum++; firstSym = true;
     } else if(firstSym === false){
-      equals()
-      console.log(evalArr)
+      equals();
       document.getElementById('output').value = (sym); nonLoc = []; numNums = 0;
       lastSym = false; lastNum = false; lastEql = false; evalArr[1] = evalArr[1] + sym; currNum++; firstSym = false;
     }
   }
 }
 function equals(){
-  let evalStr = eval(evalArr.join(''))
+  if (eval(evalArr[0]) == 0 || eval(evalArr[2]) == 0){
+    alert('Do not divide by zero!'); clearOut();
+  }
+  let evalStr = eval(evalArr.join(''));
   if (evalStr > 999999999){
     evalStr = evalStr.toExponential(9);
   }
@@ -57,7 +55,6 @@ function equals(){
 function negate(){
   if (lastSym){
   document.getElementById('output').value = (document.getElementById('output').value * -1).toLocaleString();
-  console.log(currNum);
   evalArr[currNum] = (evalArr[currNum] * -1);
   }
 }
@@ -75,12 +72,10 @@ function on(off){
   if (off == 2){
     alert('This calculator is already on! What are you doing?');
   } else{
-    alert('I\'m surpirised you did not reload.');
-    document.getElementById('main').style.visibility = 'visible';
+    alert('I\'m surpirised you did not reload.'); document.getElementById('main').style.visibility = 'visible';
   }
 }
 function off(){
   alert('You know this is an online calculator right?');
-  document.getElementById('main').style.visibility = 'hidden';
-  setTimeout(on,5000)
+  document.getElementById('main').style.visibility = 'hidden'; setTimeout(on,5000);
 }
